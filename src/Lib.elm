004eee598cd98236c97229fc3395a887104aff27
Andrey <andreyfontoura95@gmail.com>
2024-02-19 21:17:03 -0300

    add parsers and expression evaluation

--- src/Lib.elm
+++ src/Lib.elm
@@ -1,9 +1,5 @@
 module Lib exposing (..)
 import Parser exposing (..)
--- import Parser.Advanced as Adv
-import Char
-import Set
-import Html exposing (a)
 
 {-
   - Números
@@ -14,35 +10,13 @@ import Html exposing (a)
   - Invocar funções
 -}
 
--- > (+ 1 2)
--- 3
+type Op
+  = Add
+  | Mul
 
 type SExp
   = Number Int
   | Boolean Bool
   | Identifier String
   | List (List SExp)
-
-number : Parser Int
-number =
-  succeed identity
-    |= int
-
-bool : Parser Bool
-bool =
-  succeed identity
-  |. symbol "#"
-  |= oneOf
-    [
-      map (\_ -> True) (keyword "t"),
-      map (\_ -> False) (keyword "f")
-    ]
-
-identifier : Parser String
-identifier =
-  variable
-    { start = \c -> Char.isAlpha c || c == '_'
-    , inner = \c -> Char.isAlphaNum c || c == '_' || c == '-'
-    , reserved = Set.fromList ["lambda", "if", "begin", "define", "let", "and", "or"]
-    }
-
+  | Operator (Op)
